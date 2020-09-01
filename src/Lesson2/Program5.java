f724b708776465da213a74cdee884b95bd20d3a1
Anna.Klyuchuk <nusika_93@mail.ru>
2020-09-01 19:21:34 +0300

    Program 5

---
+++ src/Lesson2/Program5.java
@@ -0,0 +1,30 @@
+package Lesson2;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Program5 {
+    public static void main(String[] args) {
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Input value 1: ");
+//        int a = in.nextInt();
+//
+//        System.out.print("Input value 2: ");
+//        int b = in.nextInt();
+//
+//        System.out.print("Input value 3: ");
+//        int c = in.nextInt();
+
+        System.out.println("Введите три числа");
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[3];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        Arrays.sort(numbers);
+        System.out.println(numbers[0]);
+        sc.close();
+    }
+}
+

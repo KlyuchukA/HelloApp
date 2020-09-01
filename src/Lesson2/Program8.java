a938500f56cf20faa6d8616adbdeddb9c6b839e1
Anna.Klyuchuk <nusika_93@mail.ru>
2020-09-01 21:15:21 +0300

    Program 8 fihished

--- src/Lesson2/Program8.java
+++ src/Lesson2/Program8.java
@@ -7,27 +7,35 @@ public class Program8 {
 
         int sum = 0;
         int counter = 0;
-//        int otr = 0;
-//        int sr = 0;
+        int otr = 0;
+        int sumotr = 0;
+        int avarage = 0;
 
-        int[] a = new int[] {1,-10,5,6,45,23,-45,-34,0,32,56,-1,2,-2};
-        OptionalInt max = Arrays.stream(a).max();
-        System.out.println(max.getAsInt());
+        int[] a = new int[]{1, -10, 5, 6, 45, 23, -45, -34, 0, 32, 56, -1, 2, -2};
+//        OptionalInt max = Arrays.stream(a).max();
+//        System.out.println("Сума положительных элементов массива"+ max.getAsInt());
 
         for (int i = 0; i < a.length; i++) {
-        if (a[i] > 0) {
+            if (a[i] > 0) {
                 sum = sum + a[i];
-                counter ++;
+                counter++;
+            } else if ((a[i] < 0) & (a[i] % 2 == 0)) {
+                otr = otr + a[i];
+
+            }
+        else if (a[i] < 0) {
+                sumotr = sumotr + a[i];
+                avarage = sumotr / a.length;
+            }
         }
-        
-//        else if ((a[i] < 0) & (a[i] % 2 == 0)) {
-//            otr = otr + a[i];
-//            }
+
+            OptionalInt max = Arrays.stream(a).max();
+            System.out.println("Сума положительных элементов массива = "+ max.getAsInt());
+            System.out.println("Сума положительных элементов массива = " + sum);
+            System.out.println("Количество положительных элементов массива = " + counter);
+            System.out.println("Сумма четных отрицательных элементов = " + otr);
+            System.out.println("Среднее арифметическое отрицательных элементов = " + avarage);
         }
-    }
-    System.out.println("Сума положительных элементов массива = "+ sum);
-    System.out.println("Количество положительных элементов массива = "+ counter);
-//    System.out.println("Количество положительных элементов массива = "+otr);
     }
 
 

48955deb744f25b2530654376b5c2fcb94e68050
Anna.Klyuchuk <nusika_93@mail.ru>
2020-09-01 18:31:59 +0300

    Program 2

---
+++ src/Lesson2/Program2.java
@@ -0,0 +1,20 @@
+package Lesson2;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int num = in.nextInt();
+
+        int n1 = num / 100;
+        int n2 = (num - (n1 * 100)) / 10;
+        int n3 = num - ((n1 * 100) + (n2 * 10));
+        int n4 = n1 + n2 + n3;
+
+        System.out.println(n4);
+    }
+}

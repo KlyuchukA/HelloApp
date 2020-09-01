c506303cbf0f496190cd01df8103684e03b3ba7c
Anna.Klyuchuk <nusika_93@mail.ru>
2020-09-01 20:21:25 +0300

    Program 7

--- src/Lesson2/Program6.java
+++ src/Lesson2/Program6.java
@@ -1,12 +1,8 @@
 package Lesson2;
-
-
 import java.util.Scanner;
-
 
 public class Program6 {
     public static void main(String[] args) {
-
 
         Scanner in = new Scanner(System.in);
         System.out.print("Input a number: ");
---
+++ src/Lesson2/Program7.java
@@ -0,0 +1,26 @@
+package Lesson2;
+
+import java.util.Scanner;
+
+public class Program7 {
+
+    public static void main(String[] args) {
+        double moscow_tax = 4.15,rostov_tax = 1.98, krasnodar_tax=2.69,kirov_tax = 5;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите один из кодов города: Москва 495 или 499\n" +
+                "Ростов 863\n" +
+                "Краснодар 861\n" +
+                "Киров 8332\n");
+        int num = in.nextInt();
+
+        if ((num == 495) ^ (num == 499)) {
+            System.out.println("Москва. Стоимость разговора:" + moscow_tax*10);}
+        else if (num == 843) {
+            System.out.println("Ростов. Стоимость разговора:" + rostov_tax*10);}
+        else if (num == 861) {
+            System.out.println("Краснодар. Стоимость разговора:" + krasnodar_tax*10);}
+        else if (num == 8332) {
+            System.out.println("Киров. Стоимость разговора:" + kirov_tax*10);}
+ }
+    }
+

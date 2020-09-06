9bbf36b814de107e20f8e8c242ad3d23f8960b1d
Anna.Klyuchuk <nusika_93@mail.ru>
2020-09-06 20:38:48 +0300

    Lesson 3 Task 3 changes

--- src/Lesson32/Aspirant.java
+++ src/Lesson32/Aspirant.java
@@ -1,14 +1,15 @@
 package Lesson32;
 
-public class Aspirant extends Student {
+public class Aspirant extends Task3.Student {
     String work;
 
-    public Aspirant(String firstName, String lastName, String group, double averageMark) {
+    public Aspirant(String firstName, String lastName, String group, double averageMark, String work) {
         super(firstName, lastName, group, averageMark);
+        this.work = work;
     }
 
     public void getScholarship() {
-        if (averageMark == 5) {
+        if (this.averageMark == 5) {
             System.out.println("Сумма равна 200");
         } else {
             System.out.println("Сумма равна 180");

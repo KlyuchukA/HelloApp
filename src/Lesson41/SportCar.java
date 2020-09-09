6171aa02a96a5b5a395790aeb2fa5984dfe3aaf9
Anna.Klyuchuk <nusika_93@mail.ru>
2020-09-09 22:16:04 +0300

    Lesson 4 task2 in progress 3

--- src/Lesson41/SportCar.java
+++ src/Lesson41/SportCar.java
@@ -2,7 +2,6 @@ package Lesson41;
 
 public class SportCar extends Car{
     String speed;
-    Engine f;
 
     public SportCar (String marka, String klass, String ves, String motor, String speed, Engine f) {
         super(marka, klass, ves, motor, f);
@@ -18,7 +17,7 @@ public class SportCar extends Car{
 
     @Override
     public void printInfo() {
-        System.out.println("Грузовик: " + marka + " класс" + klass + " вес "+ ves + " двигатель " + motor + " скорость " + speed + f.power + f.performance);
+        System.out.println("Грузовик: " + marka + " класс" + klass + " вес "+ ves + " двигатель " + motor + " скорость " + speed + "сила" + this.f.power + "выполняемость" + this.f.performance);
     }
 
 

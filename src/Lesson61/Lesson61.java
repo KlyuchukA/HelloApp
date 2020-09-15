f8239066b38363db470b76a06fd8367e1b0b1517
Anna.Klyuchuk <nusika_93@mail.ru>
2020-09-15 22:12:51 +0400

    Lesson 6 in progress

---
+++ src/Lesson61/Lesson61.java
@@ -0,0 +1,27 @@
+package Lesson61;
+import java.util.ArrayList;
+import java.util.*;
+
+public class Lesson61 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add(0,"Apple");
+        list.add(1,"Banan");
+        list.add(2,"Lime");
+        list.add(3,"Grape");
+        list.add(4,"Orange");
+
+        list.add("Dragan fruit");
+        list.remove(4);
+        list.remove("Banan");
+        list.get(0);
+        list.clear();
+
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println(list.size());
+    }
+}
---
+++ src/Lesson612/Lesson612.java
@@ -0,0 +1,26 @@
+package Lesson612;
+import java.util.ArrayList;
+import java.util.*;
+
+public class Lesson612 {
+    public static void main(String[] args) {
+        List<Integer> test = new LinkedList<Integer>();
+        test.add(0, 1);
+        test.add(1, 13);
+        test.add(2, 13);
+        test.add(3, 15);
+        test.add(4, 17);
+        test.add(5, 19);
+
+        test.add(99);
+        test.remove(4);
+        test.remove((Integer)1);
+        test.get(0);
+        test.clear();
+
+        ListIterator<Integer> listIter = test.listIterator();
+        while (listIter.hasNext()) {
+            System.out.println(listIter.next());
+        }
+        System.out.println(test.size());
+    } }
